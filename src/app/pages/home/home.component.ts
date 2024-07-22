6fb453f2175c7aa2a9ce128518b46755d4a49f44
alsiesta <as@daslizenzatelier.de>
2024-07-22 21:36:06 +0200

    added fetchVideos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Category {
+  id: number;
+  name: string;
+}
+
+interface Video {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+  categories: Category[];
+}
 
 @Component({
   selector: 'app-home',
@@ -6,5 +20,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  videos: Video[] = []; 
+  constructor(private http: HttpClient) {}
 
+
+  ngOnInit() {
+    this.fetchVideos();
+  }
+
+  fetchVideos() {
+    this.http.get<Video[]>('http://127.0.0.1:8000/videos/')
+      .subscribe(data => {
+        this.videos = data;
+        console.log(this.videos); // Log the response to the console
+      });
+  }
+
+
+  scrollLeft () {
+    const container = document.getElementById('videos');
+    container!.scrollBy({ left: -200, behavior: 'smooth' });
+  }
+
+  scrollRight () {
+    const container = document.getElementById('videos');
+    container!.scrollBy({ left: 200, behavior: 'smooth' });
+  }
 }
